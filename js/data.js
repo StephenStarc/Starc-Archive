28d1371e3181c1cb7338e06ba4e26f1c0b59f4fc
Stephen Starc <stephenstarc01@gmail.com>
2024-07-31 00:01:34 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
 const data = [
-    { slno: 1, title: 'AWS - Intro', category:'AWS', date: '29 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/aws-introduction' },
-    { slno: 2, title: 'AWS - Cloud Concept', category:'AWS', date: '29 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/cloud-concepts' },
-    { slno: 3, title: 'Typesript - Intro', category:'Typescript', date: '20 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/typescript-introduction' },
+    { slno: 1, title: 'AWS - Intro', category:'AWS', date: '29 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/aws-introduction'},
+    { slno: 2, title: 'AWS - Cloud Concept', category:'AWS', date: '29 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/cloud-concepts'},
+    { slno: 3, title: 'Typesript - Intro', category:'Typescript', date: '30 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/typescript-introduction'},
+    { slno: 4, title: 'Data Structure with Js - Intro', category:'DSA', date: '31 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/data-structures-with-javascript---big-o'},
     // Add more objects as needed
 ];
