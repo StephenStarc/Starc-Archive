924fa5b4619da3566dbd93f3032761bf755d4399
Stephen Starc <stephenstarc01@gmail.com>
2024-07-30 22:24:53 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 const data = [
     { slno: 1, title: 'AWS - Intro', category:'AWS', date: '29 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/aws-introduction' },
-     { slno: 2, title: 'AWS - Cloud Concept', category:'AWS', date: '29 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/cloud-concepts' },
+    { slno: 2, title: 'AWS - Cloud Concept', category:'AWS', date: '29 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/cloud-concepts' },
+    { slno: 3, title: 'Typesript - Intro', category:'Typescript', date: '20 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/typescript-introduction' },
     // Add more objects as needed
 ];
