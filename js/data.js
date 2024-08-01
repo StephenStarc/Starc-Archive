44113c11f7b080d491641b2703b526b95c2a5de9
Stephen Starc <stephenstarc01@gmail.com>
2024-08-01 22:26:50 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -5,5 +5,6 @@ const data = [
     { slno: 4, title: 'Data Structure with Js - Intro', category:'DSA', date: '31 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/data-structures-with-javascript---big-o'},
     { slno: 5, title: 'Sun Tzu’s The Art of War - Chapter 1', category:'The Art of War', date: '31 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/sun-tzus-the-art-of-war'},
     { slno: 6, title: 'Reddit Clone - 1', category:'Next Js', date: '1 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/reddit-clone-1'},
+    { slno: 7, title: 'AWS - EC2 Instance 3', category:'AWS', date: '1 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/aws-ec-2-article-3'},
     // Add more objects as needed
 ];
