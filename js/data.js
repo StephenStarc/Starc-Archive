86322332eaef79eb274c2ea8252b73079fd5cc35
Stephen Starc <stephenstarc01@gmail.com>
2024-08-02 23:41:06 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -7,5 +7,6 @@ const data = [
     { slno: 6, title: 'Reddit Clone - 1', category:'Next Js', date: '1 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/reddit-clone-1'},
     { slno: 7, title: 'AWS - EC2 Instance 3', category:'AWS', date: '1 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/aws-ec-2-article-3'},
     { slno: 8, title: 'The Art of War - Chapter 2', category:'The Art of War', date: '2 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/sun-tzus-the-art-of-war-2'},
+    { slno: 9, title: 'The Joy Of React - 1', category:'The Joy Of React', date: '2 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/the-joy-of-react---josh-w-comeau-1'},
     // Add more objects as needed
 ];
