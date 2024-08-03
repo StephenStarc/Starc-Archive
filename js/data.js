3ec0cd269335d408dd66fc72ab794e329c200e22
Stephen Starc <stephenstarc01@gmail.com>
2024-08-04 02:25:05 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -9,5 +9,6 @@ const data = [
     { slno: 8, title: 'The Art of War - Chapter 2', category:'The Art of War', date: '2 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/sun-tzus-the-art-of-war-2'},
     { slno: 9, title: 'The Joy Of React - 1', category:'The Joy Of React', date: '2 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/the-joy-of-react---josh-w-comeau-1'},
     { slno: 10, title: 'Java Introduction', category:'Java', date: '3 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/java---introduction-1'},
+    { slno: 11, title: 'Data Structure - O(n2)', category:'DSA', date: '4 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/dsa-lesson-2'},
     // Add more objects as needed
 ];
