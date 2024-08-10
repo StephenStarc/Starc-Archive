f2885816f156abb292cca91e002c3cc40313707c
Stephen Starc <stephenstarc01@gmail.com>
2024-08-10 17:36:22 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -13,7 +13,7 @@ const data = [
     { slno: 12, title: 'Java - Lesson 2', category:'Java', date: '5 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/java---printing-and-comments-lesson-2'},
     { slno: 13, title: 'Java - Method Changed', category:'Java', date: '6 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/java-learning-method-changed'},
     { slno: 14, title: 'Java - Introduction Lesson - 3', category:'Java', date: '7 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/java-intro-brief---lesson-3'},
-    { slno: 15, title: 'Java - Variable Lesson - 4', category:'Java', date: '8 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/java-intro-brief---lesson-3'},
-     { slno: 16, title: 'upcomming', category:'frever', date: 'sorry', blogLink: 'error3'}
+    { slno: 15, title: 'Java - Variable Lesson - 4', category:'Java', date: '8 August, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/java-variables-lesson-4'},
+    { slno: 16, title: 'upcomming', category:'frever', date: 'sorry', blogLink: 'error3'}
     // Add more objects as needed
 ];
